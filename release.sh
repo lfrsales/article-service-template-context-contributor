3f24a42f5a740bf25599b823fb24dea061defae8
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-03-09 11:14:51 +0400

    Update release.

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 hub release create \
 	--attach modules/article-service-template-context-contributor/build/libs/com.liferay.demo.asset.article.service.template.context.contributor-1.0.0.jar \
-	--message 'v0.0.2' \
-	--message 'Fix LanguageId issue.' \
-	'v0.0.2'
+	--message 'v0.0.3' \
+	--message 'Add try catch to fail gently.' \
+	'v0.0.3'
