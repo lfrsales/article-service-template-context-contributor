c345ad2a1ba932894564f3f57c5a80da2868b45d
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-03-09 10:17:55 +0400

    Add release script.

---
+++ release.sh
@@ -0,0 +1,5 @@
+hub release create \
+	--attach modules/article-service-template-context-contributor/build/libs/com.liferay.demo.asset.article.service.template.context.contributor-1.0.0.jar \
+	--message 'v0.0.2' \
+	--message 'Fix LanguageId issue.' \
+	'v0.0.2'
